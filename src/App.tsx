d827256655ca012e3b7114636f41a9c062456412
Bryan Sim <bryanyf@hotmail.com>
2020-11-21 19:42:32 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Typography } from '@material-ui/core';
+import useStyles from './styles/Appstyles';
 import './App.css';
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography>This is New TS App</Typography>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/styles/Appstyles.ts
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+    tableCard: {
+      margin: 'auto',
+      height: '100%',
+      minHeight: '600px',
+      maxHeight: '800px',
+      width: '100%',
+      minWidth: '450px',
+      maxWidth: '600px',
+      borderRadius: '15px',
+    },
+  });
